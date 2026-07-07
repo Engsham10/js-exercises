62d104ff4e9902a0a6b56e8da8c253b73584f11d
EngSham10 <engsham1342@gmail.com>
2026-07-07 17:09:30 +0300

    added exercise 8 for js

---
+++ exercise8/main.js
@@ -0,0 +1,12 @@
+
+
+// creating object
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2023,
+};
+// Accessing properties
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
