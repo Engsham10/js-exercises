844fb08a422281230e85f7eb271c7454bbad5ecb
EngSham10 <engsham1342@gmail.com>
2026-07-07 15:09:12 +0300

    added exercise  4 and 5 for js

---
+++ exercise4/main.js
@@ -0,0 +1,19 @@
+
+// function declaration
+function add(a,b){
+    return a+b;
+}
+
+// calling function
+console.log(add(4,10));
+console.log(add(5,2));
+
+// function expression
+  let add = function(a,b){
+    return a+b;
+}
+// calling function
+console.log(add(3,7));
+console.log(add(2,4));
+console.log(add(4,4));
+console.log(add(5,4));
---
+++ exercise5/main.js
@@ -0,0 +1,12 @@
+
+// arrow function
+  let add = (a,b) => {
+    return a+b;
+}
+// calling function
+console.log(add(10,7));
+console.log(add(20,4));
+console.log(add(40,4));
+console.log(add(50,4));
+
+// 
