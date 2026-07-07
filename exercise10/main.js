75956a5ba116f68227695ad5c7d5434a7bed5678
EngSham10 <engsham1342@gmail.com>
2026-07-07 17:26:00 +0300

    added exercise 10 for js

---
+++ exercise10/main.js
@@ -0,0 +1,17 @@
+// creating array with object
+let library = [
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    year: 1988,
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    year: 2008
+  },
+];
+//Accessing objects inside arrays
+console.log(library[0].title);
+console.log(library[1].author);
+
