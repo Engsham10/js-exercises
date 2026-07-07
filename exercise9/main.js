7e21477dea5dba3116cc513de8d2b028caa77832
EngSham10 <engsham1342@gmail.com>
2026-07-07 17:14:31 +0300

    added exercise 9 for js

---
+++ exercise9/main.js
@@ -0,0 +1,13 @@
+
+// creating object
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2023,
+  start: () =>{
+    console.log("The car has started");
+  }
+};
+// Accessing properties
+console.log(car.start());
+
