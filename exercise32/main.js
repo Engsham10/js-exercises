2ab973ca31931f73475e0952c561f99fbd0ea5c7
EngSham10 <engsham1342@gmail.com>
2026-07-12 09:02:39 +0300

    added exercise 32 for js

---
+++ exercise32/main.js
@@ -0,0 +1,12 @@
+
+// DOM
+
+// query selector by id
+const header = document.querySelector('#header');
+console.log(header);
+// query selector all by class
+const text = document.querySelectorAll('.text');
+console.log(text);
+// query selector all by tagname
+const button = document.querySelector('button');
+console.log(button);
